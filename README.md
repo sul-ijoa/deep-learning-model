7bc0958cfc18c977ff74351c79f8cd656b0c848a
Lrbll <dlxkdxs@naver.com>
2023-12-17 17:15:55 +0900

    1st / Acc : 0.9120

--- README.md
+++ README.md
@@ -1,3 +1,93 @@
+# model#1 (pytorch 라이브러리 사용)
+
+# 변수 값 설정
+Train : transforms.RandomResizedCrop(128)
+Valid : transforms.Resize(128),
+        transforms.CenterCrop(128)
+batch_size=32
+epoch=15
+step_size=7, gamma=0.1
+
+# 결과 (약 34분 소요, 정확도 0.9120)
+Epoch 0/14
+----------
+Train Loss: 1.0536 Acc: 0.6093
+Valid Loss: 0.9376 Acc: 0.6768
+
+Epoch 1/14
+----------
+Train Loss: 0.8165 Acc: 0.6880
+Valid Loss: 0.7091 Acc: 0.7568
+
+Epoch 2/14
+----------
+Train Loss: 0.7264 Acc: 0.7309
+Valid Loss: 0.5631 Acc: 0.7768
+
+Epoch 3/14
+----------
+Train Loss: 0.6656 Acc: 0.7499
+Valid Loss: 0.5027 Acc: 0.8192
+
+Epoch 4/14
+----------
+Train Loss: 0.6187 Acc: 0.7771
+Valid Loss: 0.5677 Acc: 0.7952
+
+Epoch 5/14
+----------
+Train Loss: 0.5860 Acc: 0.7787
+Valid Loss: 0.7624 Acc: 0.7856
+
+Epoch 6/14
+----------
+Train Loss: 0.5889 Acc: 0.7909
+Valid Loss: 1.0395 Acc: 0.6952
+
+Epoch 7/14
+----------
+Train Loss: 0.4719 Acc: 0.8293
+Valid Loss: 0.2968 Acc: 0.8904
+
+Epoch 8/14
+----------
+Train Loss: 0.4047 Acc: 0.8483
+Valid Loss: 0.2679 Acc: 0.9064
+
+Epoch 9/14
+----------
+Train Loss: 0.3739 Acc: 0.8651
+Valid Loss: 0.2831 Acc: 0.8976
+
+Epoch 10/14
+----------
+Train Loss: 0.3651 Acc: 0.8680
+Valid Loss: 0.2745 Acc: 0.9000
+
+Epoch 11/14
+----------
+Train Loss: 0.3603 Acc: 0.8699
+Valid Loss: 0.2678 Acc: 0.9080
+
+Epoch 12/14
+----------
+Train Loss: 0.3354 Acc: 0.8771
+Valid Loss: 0.2525 Acc: 0.9136
+
+Epoch 13/14
+----------
+Train Loss: 0.3153 Acc: 0.8901
+Valid Loss: 0.2605 Acc: 0.9120
+
+Epoch 14/14
+----------
+Train Loss: 0.3056 Acc: 0.8899
+Valid Loss: 0.2529 Acc: 0.9120
+
+Training complete in 33m 54s
+Best Valid Acc: 0.913600
+
+
 # sul-ijoa_DeepLearning
 
 ### 이미지를 form-data로 받아서 음식인지 아닌지 판단
@@ -8,4 +98,4 @@
 **참고 사항:**  
 서버는 Flask를 기반으로 하며, 이미지 분석을 위해 Google Cloud Vision API를 활용합니다.  
 가장 높은 확률의 라벨이 "음식"인 경우, 서비스는 HTTP 200으로 응답합니다. 그렇지 않으면 HTTP 500으로 응답합니다.  
-이미지 분석 중에 오류가 발생한 경우, 자세한 내용을 포함한 오류 응답이 제공됩니다.  
+이미지 분석 중에 오류가 발생한 경우, 자세한 내용을 포함한 오류 응답이 제공됩니다.
---
+++ model#1/UsePytorch.py
@@ -0,0 +1,200 @@
+# Pytorch 라이브러리 이용
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+# Train, Valid 폴더의 상위폴더 경로
+PATH = "c:\\sulijoa_ai\\deep-learning-model\\model#1\\dataset"
+
+# GPU 활성화 상태 확인하기
+# cuda:0 == GPU, != CPU
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+# 데이터셋 전처리 (Train / Valid)
+data_transforms = {
+    'Train': transforms.Compose([
+        transforms.RandomResizedCrop(128),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'Valid': transforms.Compose([
+        transforms.Resize(128),
+        transforms.CenterCrop(128),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+# Train 폴더와 Valid 폴더의 이미지들 전처리
+image_datasets = {
+    x: datasets.ImageFolder(os.path.join(PATH, x),
+                            data_transforms[x]) for x in ['Train', 'Valid']
+}
+
+dataloaders = {
+    x: torch.utils.data.DataLoader(image_datasets[x],
+                                   batch_size=32,
+                                   shuffle=True,
+                                ) for x in ['Train', 'Valid']
+}
+
+# 이미지 개수 확인하기
+dataset_sizes = {x: len(image_datasets[x]) for x in ['Train', 'Valid']}
+print(dataset_sizes)
+
+# 이미지 학습 훈련용 데이터셋 음식 클래스명 가져오기
+class_names = image_datasets['Train'].classes
+print(class_names)
+
+# 훈련 모델
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
+
+        # 각 epoch 마다 훈련과 검증 순차적으로 진행
+        for phase in ['Train', 'Valid']:
+            if phase == 'Train':
+                model.train()  # 모델을 훈련모드로 설정
+            else:
+                model.eval()   # 모델을 평가모드로 설정
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            # 데이터 처리 반복하기?
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                #
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'Train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'Train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'Train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            # deep copy the model
+            if phase == 'Valid' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best Valid Acc: {best_acc:4f}')
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+model_1 = models.resnet18(pretrained=True) # 나중에 resnet버전 수정을 시도할 수 있을 듯
+num_ftrs = model_1.fc.in_features
+
+# Here the size of each output sample is set to 2.
+# Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+model_1.fc = nn.Linear(num_ftrs, len(class_names))
+
+model_1 = model_1.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+optimizer_sgd = optim.SGD(model_1.parameters(), lr=0.001, momentum=0.9)
+optimizer_adam = optim.Adam(model_1.parameters(), lr=0.001)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_adam, step_size=7, gamma=0.1)
+
+model_resnetft = train_model(model_1, criterion, optimizer_adam, exp_lr_scheduler,
+                       num_epochs=15)
+
+# 이미지 예측
+import torch
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def visualize_single_image(model, image_path, class_names):
+    was_training = model.training
+    model.eval()
+
+    # Load and preprocess the image
+    img = Image.open(image_path)
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    img_tensor = preprocess(img).unsqueeze(0)
+
+    # Move the tensor to the same device as the model
+    device = next(model.parameters()).device  # Get the device of the model
+    img_tensor = img_tensor.to(device)  # Move the tensor to that device
+
+    with torch.no_grad():
+        outputs = model(img_tensor)
+        _, preds = torch.max(outputs, 1)
+        confidence = torch.nn.functional.softmax(outputs, dim=1)[0]  # Get the confidence scores
+
+        # Display the image and prediction results
+        plt.figure(figsize=(8, 8))
+        plt.imshow(img)
+        plt.axis('off')
+        plt.title(f'Predicted: {class_names[preds[0]]}')
+
+        for i, (class_name, conf) in enumerate(zip(class_names, confidence.cpu())):
+            print(f"{class_name}: {conf:.2f}")
+
+        model.train(mode=was_training)
+
+
+# Specify the path to the image you want to visualize
+image_path = "c:\\sulijoa_ai\\deep-learning-model\\model#1\\dataset\\Valid\\ramen\\5025.jpg"  # Update with the actual path
+
+# Specify the class names
+class_names = image_datasets['Train'].classes  # Update with the actual class names
+
+# Visualize the single image
+visualize_single_image(model_1, image_path, class_names)
